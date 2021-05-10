265066138678af6a73f1cdf61b389887b89aa60d
playmaker <chaoleicai@gmail.com>
2021-05-10 19:56:03 +0200

    loading script done for sp-pe...

--- scripts/load_sp-pe-tb-quot-reg-2021-05-09-19h05.csv.sql
+++ scripts/load_sp-pe-tb-quot-reg-2021-05-09-19h05.csv.sql
@@ -1,24 +1,25 @@
 \echo >Loading sp-pe-tb-quot-reg-2021-05-09-19h05.csv ...
 \echo >Make sure that you are currently at projet root
-\echo
+\echo 
 \echo >Creating table, >!Old table will be droped!<
-\echo
+--\echo ;
 DROP TABLE IF EXISTS "sp-pe-tb-quot-reg-2021-05-09-19h05";
 \echo >
 CREATE TABLE "sp-pe-tb-quot-reg-2021-05-09-19h05"(
-    dep char(10),
-    red char(10),
-    fra char(110),
+    reg char(10),
     jour Date,
-    week Date,
-    pop int,
-    t int,
-    cl_age90 int,
-    p int,
-    p_h int,
-    t_h int,
     p_f int,
-    t_f int,
-    tx_std int
-)
+    p_h int,
+    P int,
+    pop_f float,
+    pop_h float,
+    cl_age90 int,
+    pop float
+);
 
+\echo Altering datestyle
+SET DATESTYLE TO YMD;
+SHOW DATESTYLE;
+
+
+\COPY "sp-pe-tb-quot-reg-2021-05-09-19h05" FROM csv_input/sp-pe-tb-quot-reg-2021-05-09-19h05.csv  DELIMITER ';'  CSV HEADER;
